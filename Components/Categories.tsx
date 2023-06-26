38d2f54ff20b6b798b9b76da6161755e09431b18
Executioner47 <faresgalal09@gmail.com>
2023-06-26 15:06:24 +0300

    (signIn | signout | add,edit,delete post | categories | filter ) added

---
+++ Components/Categories.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { categoryFilters } from "@/Constants";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+const Categories = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
+  const category = searchParams.get("category");
+  console.log(category);
+
+  const handleTags = (item: string) => {
+    if (item === "All") {
+      router.push(`${pathName}`);
+    } else {
+      router.push(`${pathName}?category=${item}`);
+    }
+  };
+
+  return (
+    <div className="flexBetween w-full gap-5 flex-wrap">
+      <ul className="flex gap-2 overflow-auto">
+        {categoryFilters.map((filter) => (
+          <button
+            key={filter}
+            type="button"
+            onClick={() => handleTags(filter)}
+            className={`${
+              category === filter || (filter === "All" && !category)
+                ? "bg-slate-200 font-medium"
+                : "font-normal"
+            } px-4 py-3 rounded-lg capitalize whitespace-nowrap`}
+          >
+            {filter}
+          </button>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,61 @@
-const Home = () => {
+import Categories from "@/Components/Categories";
+import ProjectCard from "@/Components/ProjectCard";
+import { ProjectInterface } from "@/common.types";
+import { fetchAllProjects } from "@/lib/actions";
+
+type ProjectSearch = {
+  projectSearch: {
+    edges: {
+      node: ProjectInterface;
+    }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+};
+
+type Props = {
+  searchParams: {
+    category: string | null;
+  };
+};
+
+const Home = async ({ searchParams: { category } }: Props) => {
+  const data = (await fetchAllProjects(category)) as ProjectSearch;
+
+  const projectsToDisplay = data?.projectSearch?.edges || [];
+
+  if (projectsToDisplay.length === 0) {
+    return (
+      <>
+        <section className="flexStart flex-col paddings ">
+          <Categories />
+          <p className="no-result-text text-center">
+            No projects found, go create some first.
+          </p>
+        </section>
+      </>
+    );
+  }
   return (
     <section className="flex-start flex-col paddings mb-16">
-      <h1>Categories</h1>
-      <h1>Posts</h1>
+      <Categories />
+      <section className="projects-grid">
+        {projectsToDisplay.map(({ node }: { node: ProjectInterface }) => (
+          <ProjectCard
+            key={`${node?.id}`}
+            id={node?.id}
+            image={node?.image}
+            title={node?.title}
+            name={node?.createdBy.name}
+            avatarUrl={node?.createdBy.avatarUrl}
+            userId={node?.createdBy.id}
+          />
+        ))}
+      </section>
       <h1>Loadmore</h1>
     </section>
   );
